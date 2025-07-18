0b3a2bea510b31d40735f3516f65a3de8f6492e5
watiprojects <wati.projects01@gmail.com>
2025-07-18 05:11:01 +0200

    debugging - changed color

--- alx-react-app-new/src/components/UserProfile.jsx
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -1,7 +1,7 @@
 const UserProfile = (props) => {
     return (
         <div style={{ border: '1px solid gray', padding: '10px', margin: '10px' }}>
-        <h2 style={{ color: 'pink', fontSize: '2rem', margin: '2rem' }}>{props.name}</h2>
+        <h2 style={{ color: 'blue', fontSize: '2rem', margin: '2rem' }}>{props.name}</h2>
         <p>Age: <span style={{ fontWeight: 'bold' }}>{props.age}</span></p>
         <p>Bio: {props.bio}</p>
  </div>
