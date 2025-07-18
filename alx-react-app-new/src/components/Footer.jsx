c63f22bbfda1d609e5e1ba9aedb3a1d5e5c1e4d6
watiprojects <wati.projects01@gmail.com>
2025-07-18 05:04:22 +0200

    using inline styling with React

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,12 @@
+function Footer(){
+    return(
+        <footer style ={{
+            color: 'blue',
+            fontWeight:'bold'
+        }}>
+        <p>© 2023 City Lovers</p>
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,12 @@
+function MainContent(){
+    return(
+        <main style = {{
+            fontStyle: 'italic',
+            textDecoration: 'underline'
+        }}>
+        <p>I love to visit New York, Paris, and Tokyo.</p>
+        </main>
+    );
+}
+
+export default MainContent;
