871a7b1be9062dad1885040e60e90bb2f494fd64
watiprojects <wati.projects01@gmail.com>
2025-07-25 10:07:38 +0200

    debugging move recipeStore.js

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,5 +1,5 @@
 // RecipeList component
-  import useRecipeStore from '../store/recipeStore';
+  import useRecipeStore from './recipeStore';
 
   const RecipeList = () => {
     const recipes = useRecipeStore(state => state.recipes);
