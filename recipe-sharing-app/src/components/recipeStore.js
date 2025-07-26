6ff2d5be874d7a5c4b8c7b87f8f876b95576fb6a
watiprojects <wati.projects01@gmail.com>
2025-07-26 15:48:45 +0200

    update recipeStore.js to include editing and deleting recipes

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -2,7 +2,16 @@ import { create } from 'zustand'
 
 const useRecipeStore = create(set => ({
   recipes: [],
+  //spread operator unpacks all items in an array hence writing square brackets here but not for deleteRecipe and updateRecipe as state.recipes.filter and state.recipes.map already returns an array.
   addRecipe: (newRecipe) => set(state => ({recipes: [...state.recipes, newRecipe] })),
+  
+  deleteRecipe: (recipeId) => set(state => ({
+    recipes: state.recipes.filter(recipe => recipe.id !== recipeId)
+  })),
+  //go through all recipes, if the current recipe id matches the updatedRecipe id, return the updated recipe. If not, return the original recipe unchanged.
+  updateRecipe: (updatedRecipe) => set(state => ({
+    recipes: state.recipes.map(recipe => recipe.id === updatedRecipe.id ? updatedRecipe : recipe ) 
+  })),
   setRecipes: (recipes) => set({ recipes })
 }));
 
