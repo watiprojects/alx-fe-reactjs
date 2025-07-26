ab684a74ab6aa7f197b869c228e6f1bc54754f7a
watiprojects <wati.projects01@gmail.com>
2025-07-26 15:47:46 +0200

    update recipeStore.js to include editing and deleting recipes

---
+++ alx-react-app-props/src/App.jsx
@@ -0,0 +1,10 @@
+import ProfilePage from './ProfilePage';
+import UserContext from './UserContext';
+
+function App() {
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+
+  return <ProfilePage userData={userData} />;
+}
+
+export default App;
