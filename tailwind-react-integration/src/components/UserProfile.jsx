c635d16adc9fb62a2b7701e1a263cfefb7a513e2
watiprojects <wati.projects01@gmail.com>
2025-08-10 16:33:08 +0200

    debugging

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,6 +1,6 @@
 function UserProfile() {
   return (
-    <div className="user-profile bg-gray-100 max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg p-4 md:p-8">
+    <div className="user-profile bg-gray-100 max-w-xs md:max-w-sm mx-auto my-20 rounded-lg shadow-lg p-4 sm:p-4 md:p-8">
       <img src="https://placehold.co/150" alt="User" className="rounded-full w-36 h-36 mx-auto" /> 
       <h1 className="text-xl text-blue-800 my-4">John Doe</h1>
       <p className = "text-gray-600 text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>
